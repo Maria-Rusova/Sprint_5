0fd85855ff15d6082dc50f0e733f9585b4a11160
Mariya R <KapitanProMashka@yandex.ru>
2026-04-01 12:42:49 +0300

    Add conftest, data and locators files

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from typing import Generator
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from data import Urls, StaticUser
+from locators import LoginPage, MainPage
+
+
+@pytest.fixture
+def driver() -> Generator[WebDriver, None, None]:
+    driver = webdriver.Chrome()
+    driver.get(Urls.main_page)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="session")
+def registered_user() -> StaticUser:
+    user = StaticUser.get_static()
+    temp_driver = webdriver.Chrome()
+    try:
+        StaticUser.register(temp_driver, user)
+        return user
+    finally:
+        temp_driver.quit()
+
+
+@pytest.fixture
+def login(driver: WebDriver) -> WebDriver:
+    driver.get(Urls.login_page)
+    driver.find_element(*LoginPage.EMAIL_FIELD).send_keys(StaticUser.LOGIN)
+    driver.find_element(*LoginPage.PASSWORD_FIELD).send_keys(StaticUser.PASSWORD)
+    driver.find_element(*LoginPage.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located(MainPage.ORDER_BUTTON))
+    return driver
